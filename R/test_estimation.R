59c32d8c11b22a96ce33bff9b2d82ccd8d689b5e
Keith O'Hara <kthohr@users.noreply.github.com>
2017-09-22 04:36:24 -0400

    remove test [ci skip]

--- R/test_estimation.R
+++ R/test_estimation.R
@@ -21,85 +21,85 @@
 ##
 ################################################################################
 
-test_loglikelihood <- function(seed=777, nbX=5, nbY=4, dX=3, dY=2)
-{  
-    set.seed(seed)
-    tm = proc.time()
-    #
-    message('*===================   Start of test_loglikelihood   ===================*\n')
-    #
+# test_loglikelihood <- function(seed=777, nbX=5, nbY=4, dX=3, dY=2)
+# {  
+#     set.seed(seed)
+#     tm = proc.time()
+#     #
+#     message('*===================   Start of test_loglikelihood   ===================*\n')
+#     #
     
-    n = rep(1,nbX)
-    m = rep(1,nbY)  
+#     n = rep(1,nbX)
+#     m = rep(1,nbY)  
     
-    xs = matrix(runif(nbX*dX),nrow=nbX)
-    ys = matrix(runif(nbY*dY),nrow=nbY)
+#     xs = matrix(runif(nbX*dX),nrow=nbX)
+#     ys = matrix(runif(nbY*dY),nrow=nbY)
     
-    #
+#     #
 
-    logitM = new(logit)
-    logitW = new(logit)
+#     logitM = new(logit)
+#     logitW = new(logit)
 
-    logitM$build(nbX,nbY)
-    logitW$build(nbY,nbX)
+#     logitM$build(nbX,nbY)
+#     logitW$build(nbY,nbX)
 
-    A <- matrix(1,nrow=dX,ncol=dY)
-    phi <- xs %*% A %*% t(ys) 
+#     A <- matrix(1,nrow=dX,ncol=dY)
+#     phi <- xs %*% A %*% t(ys) 
     
-    mktSim <- new(dse_empirical_tu)
-    mktSim$build(n,m,phi,logitM,logitW,FALSE)
+#     mktSim <- new(dse_empirical_tu)
+#     mktSim$build(n,m,phi,logitM,logitW,FALSE)
 
-    #
+#     #
 
-    res <- cupids_lp(mktSim)
+#     res <- cupids_lp(mktSim)
 
-    mu_hat <- res$mu
+#     mu_hat <- res$mu
 
-    mu_hat_x0 <- n - apply(mu_hat,1,sum)
-    mu_hat_0y <- m - apply(mu_hat,2,sum)
+#     mu_hat_x0 <- n - apply(mu_hat,1,sum)
+#     mu_hat_0y <- m - apply(mu_hat,2,sum)
 
-    #
+#     #
 
-    muhat = CupidsLP(mktSim, T, F)$mu
-    muhatx0  = n-apply(muhat,1,sum)
-    muhat0y  = m-apply(muhat,2,sum)
-    #
-    TUlogitmodel = buildModel_TU_logit(array(kronecker(ys,xs) , dim=c(nbX,nbY,dX*dY) ),n,m)
-    theta0=initparam(TUlogitmodel)$param
-    market = parametricMarket(TUlogitmodel,theta0)
-    dtheta = diag(TUlogitmodel$nbParams)
-    #dtheta = matrix(0.1,nrow=6,ncol=1)
-    #
-    tsf = proc.time()  
-    #mudmu = dtheta_mu_default(TUlogitmodel,market,theta0,dtheta)
-    mudmu = dtheta_mu_default(TUlogitmodel,market,theta0)
-    message(paste0('Time elapsed - general: ', round((proc.time()-tsf)["elapsed"],5), 's.')) 
-    #
-    tsf = proc.time()  
-    dmunum = dtheta_mu_numeric(TUlogitmodel,market,theta0,dtheta)
-    message(paste0('Time elapsed - numerical: ', round((proc.time()-tsf)["elapsed"],5), 's.')) 
-    #
-    tsf = proc.time()    
-    dmulogit=dtheta_mu_logit(TUlogitmodel,market,theta0,dtheta)
-    message(paste0('Time elapsed - logit: ', round((proc.time()-tsf)["elapsed"],5), 's.')) 
-    #
-    mu = mudmu$mu 
-    #
-    LL = sum(2* muhat * log(mudmu$mu)) + sum(muhatx0 * log(mudmu$mux0s)) + sum(muhat0y * log(mudmu$mu0ys)) 
-    gradLL = apply( c( t( t(2* muhat/matrix(mudmu$mu,nrow=nbX) - muhatx0 / mudmu$mux0s)  - muhat0y / mudmu$mu0ys ) )* mudmu$dmu   ,2,sum) 
-    gradLLlogit = apply( c( t( t(2* muhat/matrix(dmulogit$mu,nrow=nbX) - muhatx0 / dmulogit$mux0s)  - muhat0y / dmulogit$mu0ys ) )* dmulogit$dmu   ,2,sum) 
-    gradLLnum = apply( c( t( t(2* muhat/matrix(dmunum$mu,nrow=nbX) - muhatx0 / dmunum$mux0s)  - muhat0y / dmunum$mu0ys ) )* dmunum$dmu   ,2,sum)
-    #
-    print(gradLL)
-    print(gradLLlogit)
-    print(gradLLnum)
-    #
-    time = proc.time() - tm  
-    message(paste0('\nEnd of test_loglikelihood. Time elapsed = ', round(time["elapsed"],5), 's.\n'))
-    #
-    ret <- c(LL,gradLL,gradLLlogit,gradLLnum)
-    return(ret)
-}
+#     muhat = CupidsLP(mktSim, T, F)$mu
+#     muhatx0  = n-apply(muhat,1,sum)
+#     muhat0y  = m-apply(muhat,2,sum)
+#     #
+#     TUlogitmodel = buildModel_TU_logit(array(kronecker(ys,xs) , dim=c(nbX,nbY,dX*dY) ),n,m)
+#     theta0=initparam(TUlogitmodel)$param
+#     market = parametricMarket(TUlogitmodel,theta0)
+#     dtheta = diag(TUlogitmodel$nbParams)
+#     #dtheta = matrix(0.1,nrow=6,ncol=1)
+#     #
+#     tsf = proc.time()  
+#     #mudmu = dtheta_mu_default(TUlogitmodel,market,theta0,dtheta)
+#     mudmu = dtheta_mu_default(TUlogitmodel,market,theta0)
+#     message(paste0('Time elapsed - general: ', round((proc.time()-tsf)["elapsed"],5), 's.')) 
+#     #
+#     tsf = proc.time()  
+#     dmunum = dtheta_mu_numeric(TUlogitmodel,market,theta0,dtheta)
+#     message(paste0('Time elapsed - numerical: ', round((proc.time()-tsf)["elapsed"],5), 's.')) 
+#     #
+#     tsf = proc.time()    
+#     dmulogit=dtheta_mu_logit(TUlogitmodel,market,theta0,dtheta)
+#     message(paste0('Time elapsed - logit: ', round((proc.time()-tsf)["elapsed"],5), 's.')) 
+#     #
+#     mu = mudmu$mu 
+#     #
+#     LL = sum(2* muhat * log(mudmu$mu)) + sum(muhatx0 * log(mudmu$mux0s)) + sum(muhat0y * log(mudmu$mu0ys)) 
+#     gradLL = apply( c( t( t(2* muhat/matrix(mudmu$mu,nrow=nbX) - muhatx0 / mudmu$mux0s)  - muhat0y / mudmu$mu0ys ) )* mudmu$dmu   ,2,sum) 
+#     gradLLlogit = apply( c( t( t(2* muhat/matrix(dmulogit$mu,nrow=nbX) - muhatx0 / dmulogit$mux0s)  - muhat0y / dmulogit$mu0ys ) )* dmulogit$dmu   ,2,sum) 
+#     gradLLnum = apply( c( t( t(2* muhat/matrix(dmunum$mu,nrow=nbX) - muhatx0 / dmunum$mux0s)  - muhat0y / dmunum$mu0ys ) )* dmunum$dmu   ,2,sum)
+#     #
+#     print(gradLL)
+#     print(gradLLlogit)
+#     print(gradLLnum)
+#     #
+#     time = proc.time() - tm  
+#     message(paste0('\nEnd of test_loglikelihood. Time elapsed = ', round(time["elapsed"],5), 's.\n'))
+#     #
+#     ret <- c(LL,gradLL,gradLLlogit,gradLLnum)
+#     return(ret)
+# }
 
 test_mle <- function(seed=777, nbX=80, nbY=72, noiseScale=0.1, dX=3, dY=3)
 {  
